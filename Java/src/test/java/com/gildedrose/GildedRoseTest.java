d93d666b3dd3070e4c3383391fa2fb22daf972fe
Jan Pralle <jan.pralle@netpioneer.de>
2017-11-23 10:56:49 +0100

    Moved updateQuality and test fixture to main method to enable legacy refactoring vs no tests trap.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -9,9 +9,8 @@ public class GildedRoseTest {
     @Test
     public void foo() {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        Main.updateQuality(items);
+        assertEquals("fixme", items[0].name);
     }
 
 }
